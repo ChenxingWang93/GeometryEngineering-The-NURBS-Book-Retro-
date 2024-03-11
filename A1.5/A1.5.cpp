c2ead920697befc57e78c2bfb34fd043966cacf7
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-03-11 10:45:09 +0800

    .cpp

---
+++ A1.5/A1.5.cpp
@@ -0,0 +1,23 @@
+void deCasteljau1(double* P, int n, double u, double* C) {
+    double Q[n+1];
+
+    // Copy P into Q
+    for (int i = 0; i <= n; i++)
+    {
+        /* code */
+        Q[i] = P[i];
+    }
+
+    for (int k = 1; k <= n; k++)
+    {
+        /* code */
+        for (int i = 0; i <= n - k; i++)
+        {
+            /* code */
+            Q[i] = (1.0 - u) * Q[i] + u * Q[i + 1];
+        }
+    }
+    
+    C[0] = Q[0];
+    
+}
