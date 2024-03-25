34a01e44d6070b60ab2049dbde5e4c8f1e3a2195
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-03-26 00:00:33 +0800

    .html

--- A1.6/A1.6.cpp
+++ A1.6/A1.6.cpp
@@ -1,3 +1,13 @@
+void Horner1(double a[], int m, double x, double result)
+{
+    result = a[m];
+    for (int i = m - 1; i >= 0; i--)
+    {
+        /* code */
+        result = result * x + a[i];
+    }
+}
+
 void Horner2(double** a, int n, int m, double u0, double v0, double S) {
     double b[n+1];
     for (int i = 0; i <= n; i++)
