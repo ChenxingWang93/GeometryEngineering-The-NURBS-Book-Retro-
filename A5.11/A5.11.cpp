308d7c07fe2c34ac1477c6a2568449ad395482d3
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-04-24 23:50:43 +0800

    update: .cpp file

---
+++ A5.11/A5.11.cpp
@@ -0,0 +1,149 @@
+void DegreeReduceCurve(int n, int p, double* U, double* Qw, int nh, double* Uh, double* Pw) {
+    int ph = p - 1;
+    int mh = ph;
+    int kind = ph + 1;
+    int r = -1;
+    int a = p;
+    int b = p + 1;
+    int cind = 1;
+    int mult = p;
+    int m = n + p + 1;
+    Pw[0] = Qw[0];
+
+    double Uh[ph+1];
+    double bpts[p+1];
+    double e[m];
+    double alphas[p];
+    double rbpts[p];
+
+    for (int i = 0; i <= ph; i++)
+        Uh[i] = U[0];
+
+    for (int i = 0; i <= p; i++)
+        bpts[i] = Qw[i];
+    
+    for (int i = 0; i < m; i++)
+        e[i] =  0.0;
+    
+    while (b < m)
+    {
+        int i = b;
+        while (b < m && U[b] == U[b + 1])
+            b = b + 1;
+        
+        int mult = b - i + 1;
+        mh = mh + mult - 1;
+        int oldr = r;
+        r = p - mult;
+
+        if (oldr > 0)
+            lbz = (oldr + 2) / 2;
+        else
+            lbz = 1;
+
+        if (r > 0)
+        {
+            double numer = U[b] - U[a];
+            for (int k = p; k >= mult; k--)
+                alphas[k - mult - 1] = numer / (U[a + k] - U[a]);
+            for (int j = 1; j <= r; j++)
+            {
+                int save = r - j;
+                int s = mult + j;
+                for (int k = p; k >= s; k--)
+                    bpts[k] = alphas[k - s] * bpts[k] + (1.0 - alphas[k - s]) * bpts[k - 1];
+                Nextbpts[save] = bpts[p];
+            }
+        }
+
+        /* Degree reduce Bezier segment */
+        BezDegreeReduce(bpts, rbpts, MaxErr);
+        e[a] = e[a] + MaxErr;
+
+        if (e[a] > TOL)
+            /* Curve not degree reducible */
+            return 1;
+        
+        /* Remove knot U[a] older times */
+        if (oldr > 0)
+        {
+            int first = kind;
+            int last = kind;
+            for (int k = 0; k < oldr; k++)
+            {
+                int i = first;
+                int j = last;
+                int kj = j - kind;
+                while (j - i > k)
+                {
+                    double alfa = (U[a] - Uh[i - 1]) / (U[b] - Uh[i - 1]);
+                    double beta = (U[a] - Uh[j - k - 1]) / (U[b] - Uh[j - k - 1]);
+                    Pw[i - 1] = (Pw[i - 1] - (1.0 - alfa) * Pw[i - 2]) / alfa;
+                    rbpts[kj] = (rbpts[kj] - beta * rbpts[kj + 1]) / (1.0 - beta);
+                    i = i + 1;
+                    j = j - 1;
+                    kj = kj - 1;
+                }
+
+                /* Compute knot removal error bounds (Br) */
+                double Br;
+                if (j - i < k)
+                    Br = Distance4D(Pw[i - 2], rbpts[kj + 1]);
+                else {
+                    double delta = (U[a] - Uh[i - 1]) / (U[b] - Uh[i - 1]);
+                    double A = delta * rbpts[kj + 1] + (1.0 - delta) * Pw[i - 2];
+                    Br = Distance4D(Pw[i - 1], A);
+                }
+                
+                /* Update the error vector */
+                int K = a + oldr - k;
+                int q = (2 * q - k + 1) / 2;
+                int L = K - q;
+                for (int ii = L; ii <= a; ii++)
+                {
+                    e[ii] = e[ii] + Br;
+                    if (e[ii] > TOL)
+                        return 1;
+                }
+
+                first = first - 1;
+                last = last + 1;
+            }
+            cind = i - 1;
+        }
+
+        /* Load knot vector and control points */
+        if (a != p)
+        {
+            for (int i = 0; i < ph - oldr; i++)
+            {
+                Uh[kind] = U[a];
+                kind = kind + 1;
+            }
+        }
+        
+        for (int i = lbz; i <= ph; i++)
+        {
+            Pw[cind] = rbpts[i];
+            cind = cind + 1;
+        }
+
+        if (b < m)
+        {
+            for (int i = 0; i < r; i++)
+                bpts[i] = Nextbpts[i];
+                cind = cind + 1;
+            for (int i = r; i <= p; i++)
+                bpts[i] = Qw[b - p + i];
+
+            a = b;
+            b = b + 1;
+        } else {
+            for (int i = 0; i <= ph; i++)
+                Uh[kind + i] = U[b];
+        }
+    }
+
+    nh = mh - ph - 1;
+    return 0;
+}
---
+++ A5.9/A5.9.cpp
@@ -0,0 +1,128 @@
+using namespace std;
+
+void DegreeElevateCurve(int n, int p, double* U, double* Pw, int t, int& nh, double* Uh, double* Qw) {
+    int m = n + p + 1;
+    int ph = p + t;
+    int ph2 = ph / 2;
+
+    double bezalfs[MAX_SIZE][MAX_SIZE];
+    double alfs[MAX_SIZE];
+    double bpts[MAX_SIZE];
+    double Nextbpts[MAX_SIZE];
+    double ebpts[MAX_SIZE];
+
+    /* Compute Bezier degree elevation coefficients */
+    bezalfs[0][0] = bezalfs[ph][p] = 1.0;
+    for (int i = 1; i < ph2; i++)
+    {
+        double inv = 1.0 / Bin(ph, i);
+        int mpi = std::min(p, i);
+        for (int i = std::max(0, i - t); j <= mpi; j++)
+        {
+            bezalfs[i][j] = bezalfs[ph - i][p - j];
+        }
+    }
+    
+    int mh = ph;
+    int kind = ph + 1;
+    int r = -1;
+    int a = p;
+    int b = p + 1;
+    int cind = 1;
+    double ua = U[0];
+    Qw[0] = Pw[0];
+
+    for (int i = 0; i <= ph; i++)
+    {
+        Uh[i] = ua;
+    }
+
+    for (int i = 0; i <= ph; i++)
+    {
+        bpts[i] = Pw[i];
+    }
+    
+    while (b < m)
+    {
+        int i = b;
+        while (b < m && U[b] == U[b + 1])
+        {
+            b = b + 1;
+        }
+        
+        int mul = b - i + 1;
+        mh = mh + mul + t;
+        double ub = U[b];
+        int oldr = r;
+        r = p - mul;
+
+        int lbz, rbz;
+        if (oldr > 0)
+        {
+            lbz = (oldr + 2) / 2;
+        } else {
+            lbz = 1;
+        }
+        
+        if (r > 0)
+        {
+            rbz = ph - (r + 1) / 2;
+        } else {
+            rbz = ph;
+        }
+        
+        /* Insert knot to get Bezier segment */
+        if (r > 0)
+        {
+            double numer = ub - ua;
+            for (int k = p; k > mul; k--)
+            {
+                alfs[k - mul - 1] = numer / (U[a + k] - ua);
+            }
+
+            for (int j = 1; j <= r; j++)
+            {
+                double save = r - j;
+                int s = mul + j;
+                for (int k = p; k >= s; k--)
+                {
+                    bpts[k] = alfs[k - s] * bpts[k] + (1.0 - alfs[k - s]) * bpts[k - 1];
+                }
+                Nextbpts[save] = bpts[p];
+            }
+        }
+
+        for (int i = lbz; i <= ph; i++)
+        {
+            ebpts[i] = 0.0;
+            int mpi = std::min(p, i);
+            for (int j = std::max(0, i - t); j <= mpi; j++)
+            {
+                ebpts[i] += bezalfs[i][j] * bpts[j];
+            }
+        }
+
+        /* Further implementation for knot removal and loading control points into Qw can be added here */
+        if (b < m)
+        {
+            for (int j = 0; j < r; j++)
+            {
+                bpts[j] = Nextbpts[j];
+            }
+            for (int j = r; j <= p; j++)
+            {
+                bpts[j] = Pw[b - p + j];
+            }
+            a = b;
+            b = b + 1;
+            ua = ub;
+        } else {
+            for (int i = 0; i <= ph; i++)
+            {
+                Uh[kind + i] = ub;
+            }
+        }
+    }
+
+    nh = mh - ph - 1;
+}
