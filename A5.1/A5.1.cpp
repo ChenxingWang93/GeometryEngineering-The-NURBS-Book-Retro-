cf8c1930605ed6b4fcb30cc6fa9ab25cd4576d71
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-04-25 17:34:13 +0800

    fix: no problems in .cpp file

--- A5.1/A5.1.cpp
+++ A5.1/A5.1.cpp
@@ -15,9 +15,7 @@
  *         `Qw`        -    control points of the new curve.
  * 
  * Steps:
- *     
  *     i.initialize Variables:
- *         
  *         `mp`        -    total number of control points including the original ones and those to be inserted.
  *         `np`        -    set as the number of control points in the new curve after knot insertion.
  *     
@@ -42,11 +40,11 @@ void CurveKnotIns(int np, int p, vector<double>& UP, vector<double>& Pw, double 
     int mp = np + p + 1;
     nq = np + r;
 
-    /* Temporary array for control points 控制点的临时 阵列 */
+    /* Temporary array for control points 控制點的臨時 陣列 */
     vector<double> Rw(p + 1);
 
-    /* Load new knot vector 加载新 的结点向量 */
-    for (int i = ; i <= k - p; i++)
+    /* Load new knot vector 加載新 的結點向量 */
+    for (int i = 0; i <= k - p; i++)
     {
         Qw[i] = Pw[i];
     }
@@ -54,28 +52,30 @@ void CurveKnotIns(int np, int p, vector<double>& UP, vector<double>& Pw, double 
     {
         Qw[i + r] = Pw[i];
     }
-    for (int i = ; i <= p - s; i++)
+
+    vector<double> Rw(p - s + 1);
+    for (int i = 0; i <= p - s; i++)
     {
         Rw[i] = Pw[k - p + i];
     }
 
     for (int j = 1; j <= r; j++)
     {
-        /* Insert the knot r times 迭代结点 r 次 */
+        /* Insert the knot r times 插入結點 r 次 */
         int L = k - p + j;
-        for (int i = ; i <= p - j - s; i++)
+        for (int i = 0; i <= p - j - s; i++)
         {
             double alpha = (u - UP[L + i]) / (UP[i + k + 1] - UP[L + i]);
             Rw[i] = alpha * Rw[i + 1] + (1. - alpha) * Rw[i];
         }
-        Qw[L] = Rw[];
+        Qw[L] = Rw[0];
         Qw[k + r - j - s] = Rw[p - j - s];
     }
     
     int L = k - p + r;
     for (int i = L + 1; i < k - s; i++)
     {
-        /* Load remaining control points 加载剩余的 控制点 */
+        /* Load remaining control points 加載剩餘的 控制點 */
         Qw[i] = Rw[i - L];
     }
 }
