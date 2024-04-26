201cd85b7da0f106008e6985b86b1301cfe8841f
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-04-26 22:34:19 +0800

    update: notes in .cpp file

--- A5.5/A5.5.cpp
+++ A5.5/A5.5.cpp
@@ -1,3 +1,39 @@
+/* ARGUMENTS
+ *     Input Arguments:
+ *         `n`        -    Number of control points minus one in the u-direction.
+ *         `p`        -    Degree of the B-spline curve in the u-direction.
+ *         `U`        -    Knot vector of the B-spline curve in the u-direction.
+ *         `m`        -    Number of control points minus one in the v-direction.
+ *         `q`        -    Degree of the B-spline curve in the v-direction.
+ *         `V`        -    Knot vector of the B-spline curve in the v-direction.
+ *         `Pw`       -    Control points of the surface.
+ *         `X`        -    Array containing the knot values to be inserted.
+ *         `r`        -    Number of times each knot is to be inserted.
+ *         `dir`      -    Direction indicator(whether to refine in the u-direction or v-direction).
+ * 
+ *     Outputs Arguments:
+ *         `Ubar`     -    Refined knot vector of the surface in the u-direction.
+ *         `Vbar`     -    Refined knot vector of the surface in the v-direction.
+ *         `Qw`       -    Refined control points of the surface.
+ * 
+ * Steps:
+ *     i.Initialization:
+ *         Find indexes `a` and `b` corresponding to the knot values in the u-direction.
+ *         Initialize `Ubar`.
+ *         Copy the knot vector `V` into `Vbar`.
+ *         Save unaltered control points into `Qw`.
+ * 
+ *     ii.Refining in the U-direction:
+ *         Iterate over each knot value in `X` in reverse order.
+ *         While the current knot value is less than or equal to `U[i]` and `i` is greater than `a`, insert control points and refine the knot vector:
+ *             Update control points and refine the knot vector using the de Boor algorithm.
+ *             Decrement `k` and `i`.
+ *         Insert the remaining control points using the de Boor algorithm.
+ * 
+ *     iii.Refining in the V-direction (similar steps as in the U-direction).
+ *         
+ */
+
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -6,11 +42,11 @@ using namespace std;
 void RefineKnotVectSurface(int n, int p, vector<double>& U, int m, int q, vector<double>& V, vector<vector<double>>& Pw, vector<double>& X, int r, int dir, vector<double>& Ubar, vector<double>& Vbar, vector<vector<double>>& Qw) {
     if (dir == 0)
     {
-        /* Refining in U direction */
+        /* Refining in U direction 在 U-方向 精制 */
         int a, b;
         
-        /* Find indexes a and b */
-        /* Save unaltered control points */
+        /* Find indexes a and b 找到 a &b指针 */
+        /* Save unaltered control points 保存 不变的 控制点 */
         for (int row = 0; row <= m; row++)
         {
             for (int k = 0; k <= a - p; k++)
@@ -27,7 +63,7 @@ void RefineKnotVectSurface(int n, int p, vector<double>& U, int m, int q, vector
             int i = n + 1;
             while (X[j] <= U[i] && i > a)
             {
-                /* Compute Ubar */
+                /* Compute Ubar 计算 U-bar */
                 int k = n + p;
                 for (int row = 0; row <= m; row++)
                 {
@@ -52,7 +88,7 @@ void RefineKnotVectSurface(int n, int p, vector<double>& U, int m, int q, vector
                         Qw[ind - 1][row] = Qw[ind][row];
                     } 
                 } else {
-                    /* Compute alfa */
+                    /* Compute alfa 计算alfa */
                     for (int row = 0; row <= m; row++)
                     {
                         Qw[ind - 1][row] = alfa * Qw[ind - 1][row] + (1.0 - alfa) * Qw[ind][row];
