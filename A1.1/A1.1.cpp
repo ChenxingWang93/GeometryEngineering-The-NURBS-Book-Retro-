b869ed5ba467380d97242feb68494a441f34632b
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2024-04-28 23:23:29 +0800

    add: notes in .cpp files

--- A1.1/A1.1.cpp
+++ A1.1/A1.1.cpp
@@ -1,10 +1,34 @@
+/* ARGUMENTS
+ *     Input Arguments:
+ *         `a`        -    An array storing the coefficients of a polynomial of degree `n`.
+ *         `n`        -    The degree of the polynomial.
+ *         `u0`       -    The value at which the polynomial needs to be evaluated.
+ *     
+ *     Outputs Arguments:
+ *         `C`        -    The result of evaluating the polynomial at the point `u0`.
+ * 
+ * Steps:
+ *     i.Initialize `C` to be the coefficient of the highest degree term in the polynomial, i.e., `C = a[n]`.
+ * 
+ *     ii.Starting from the next highest degree term, iterate through the coefficients in reverse order(from `n-1` to `0`).
+ * 
+ *     iii.For each coefficient `a[i]`, update `C` using the Horner's method:
+ *           Multiply the current value of `C` by `u0`.
+ *           Add the next coefficient `a[i]`.
+ *     iv.Repeat this process for all coefficients from the second highest degree down to the constant term.
+ * 
+ *     v.After iterating through all coefficients, `C` will hold the value of the polynomial evaluated at the point `u0`.
+ * 
+ */
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 /* Function to compute point on power basis curve using Horner's method */
-double Horner1(vector<double>& a, int n, double u0) {
+double Horner1(vector<double>& a, int n, double u0, double& C) {
     double C = a[n];
+
     for (int i = n - 1; i >= 0; i--)
     {
         C = C * u0 + a[i];
@@ -13,11 +37,17 @@ double Horner1(vector<double>& a, int n, double u0) {
 }
 
 int main() {
-    /* Example usage */
-    vector<double> coefficients = {1, 2, 3};    /* Example coefficients */
-    int degree = 2;     /* Example degree */
-    double point = 2.5;     /* Example point */
-    double result = Horner1(coefficients, degree, point);
-    cout << "Result: " << result << endl;
+    /* Example input data */
+    vector<double> coefficients = {1.0, 2.0, 3.0, 4.0};    /* Example coefficients of the power basis curve */
+    int n = a.size() - 1;    /* Example degree of the curve */
+    double u0 = 2.5;    /* Parameter value */
+    double C;    /* Output point on the curve */
+
+    /* Call the Horner1 function */
+    Horner1(a, n, u0, C);
+
+    /* Output the result */
+    cout << "Point on the power basis curve at u0= " << u0 << ": " << C << endl;
+    
     return 0;
 }
--- A1.2/A1.2.cpp
+++ A1.2/A1.2.cpp
@@ -1,17 +1,70 @@
-void Bernstein(int i, int n, double u, double B) {
-    double temp[n+1];
-    double u1 = 1.0 - u;
+/* ARGUMENTS
+ *     Input Arguments:
+ *         `i`        -    The index of the Bernstein polynomial term.
+ *         `n`        -    The degree of the Bernstein polynomial.
+ *         `u`        -    The parameter at which the Bernstein polynomial is to be evaluated.
+ * 
+ *     Outputs Arguments:
+ *         `B`        -    The value of the Bernstein polynomial at the given parameter u`.
+ * 
+ * Steps:
+ *     i. Initialize an array `temp` of size `n+1` to store intermediate values of the Bernstein polynomail.
+ * 
+ *     ii. Initialize all elements of the `temp` array to 0.0.
+ * 
+ *     iii. Set `temp[n-i]` to 1.0. This step initializes the array to represent the Bernstein polynomial term corresponding to index `i`.
+ * 
+ *     iv. Calculate `u1 = 1.0 - u`. This will be used in the subsequent calculations.
+ * 
+ *     v. Iterate over `k` from 1 to `n`:
+ *           - For each `k`,iterate over `j` from `n` down to `k`:
+ *               - Update `temp[j]` using the formula: `temp[j] = u1 * temp[j] + u * temp[j-1]`.
+ *               - This process updates the values in the `temp` array based on the parameter `u`.
+ * 
+ *     vi. After the loops, the value of the Bernstein polynomial at parameter `u` is stored in `B = temp[n]`.
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Bernstein(int i, int n, double u, double& B) {
+    /* Temporary array to store values */
+    vector<double> temp(n + 1, 0.0);
+
+    /* Initialize the temporary array */
+    temp[n + i] = 1.0;
 
     for (int j = 0; j <= n; j++) {
         temp[j] = 0.0;
     }
-    temp[n - i] = 1.0;
 
+    temp[n - i] = 1.0;
+    double u1 = 1.0 - u;
+
+    /* Compute the values of the Bernstein polynomial */
     for (int k = 1; k <= n; k++) {
         for (int j = n; j >= k; j--) {
             temp[j] = u1 * temp[j] + u * temp[j - 1];
         }
     }
     
+    /* Set the output value B to the computed value at u */
     B = temp[n];
+}
+
+int main() {
+    int i = 2;    /* Example value of i */
+    int n = 5;    /* Example value of n */
+    double u = 0.3;    /* Example value of u */
+    double B;    /* Output value of the Bernstein polynomial */
+
+    /* Call the Bernstein function */
+    Bernstein(i, n, u, B);
+
+    /* Output the result */
+    cout << "The value of the Bernstein polynomial at u = " << u << " is: " << B << endl;
+
+    return 0;
 }
